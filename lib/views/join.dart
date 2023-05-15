649fa349e791eda59551dcdcc5ec95f9e0de402c
aalohaa <khusainov.alizhan@bk.ru>
2023-05-16 01:17:08 +0600

    commit
    join functionality

--- lib/views/join.dart
+++ lib/views/join.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class Join extends StatefulWidget {
   const Join({super.key});
@@ -8,189 +10,299 @@ class Join extends StatefulWidget {
 }
 
 class _JoinState extends State<Join> {
-  
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  final TextEditingController _dashboardIdController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  bool isLoading = false;
+
+  void _joinDashboard() async {
+    final isValid = _formKey.currentState!.validate();
+    if (!isValid || isLoading) {
+      return;
+    }
+
+    setState(() { // new
+      isLoading = true;
+    });
+
+    final dashboardId = _dashboardIdController.text;
+    final email = _emailController.text;
+    final password = _passwordController.text;
+
+    // Check if dashboard exists
+    final dashboardSnapshot =
+        await _firestore.collection('dashboards').doc(dashboardId).get();
+    if (!dashboardSnapshot.exists) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Invalid dashboard ID'),
+        ),
+      );
+
+       setState(() { // new
+        isLoading = false;
+      });
+      return;
+    }
+    
+
+    try {
+      // Join dashboard and add user to members collection
+      final userCredential =
+          await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      final user = userCredential.user!;
+      final membersRef = _firestore
+          .collection('dashboards')
+          .doc(dashboardId)
+          .collection('members');
+      await membersRef.doc(user.uid).set({
+        'email': email,
+        'password': password,
+        'createdAt': FieldValue.serverTimestamp(),
+      });
+
+      // Show success message and clear form
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Joined dashboard successfully'),
+        ),
+      );
+      _formKey.currentState!.reset();
+       Navigator.pushReplacementNamed(context, '/main_view');
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'email-already-in-use') {
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: const Text('Error'),
+              content: const Text('The email address is already in use.'),
+              actions: <Widget>[
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text('OK'),
+                ),
+              ],
+            );
+          },
+        );
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text('Error occurred. Please try again later.'),
+          ),
+        );
+      }
+       setState(() { // new
+        isLoading = false;
+      });
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Error occurred. Please try again later.'),
+        ),
+      );
+     setState(() { // new
+        isLoading = false;
+      });
+    }
+  }
+
   final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     final ButtonStyle style =
         ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20));
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      backgroundColor: const Color.fromARGB(255, 149, 152, 229),
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(MediaQuery.of(context).padding.top),
-        child: SizedBox(
-        height: MediaQuery.of(context).padding.top,
-        ),
-      ),
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/background.png"),
-            fit: BoxFit.fill,
-            colorFilter: ColorFilter.mode(Color.fromARGB(255, 149, 152, 229), BlendMode.overlay)
+        resizeToAvoidBottomInset: false,
+        backgroundColor: const Color.fromARGB(255, 149, 152, 229),
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(MediaQuery.of(context).padding.top),
+          child: SizedBox(
+            height: MediaQuery.of(context).padding.top,
           ),
         ),
-        child:  Column(
-          children: [ GestureDetector( onTap: () {
-            Navigator.pop(context); 
-            },
-              child: const Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                 Padding(
-                  padding: EdgeInsets.fromLTRB(20, 20, 0, 0),
-                  child:  Image(image: AssetImage("assets/back_arrow.png")),
-                  )
-               
-              ]
-            )),
-            const SizedBox(
-              width: 0,
-              height: 30,
+        body: Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("assets/background.png"),
+                  fit: BoxFit.fill,
+                  colorFilter: ColorFilter.mode(
+                      Color.fromARGB(255, 149, 152, 229), BlendMode.overlay)),
             ),
-            const Row(
-              
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("HomeMate", style: TextStyle(
-                  fontSize: 45,
-                  fontFamily: 'Poppins',
-                  color: Colors.white,
-                  shadows: <Shadow>[
-                    Shadow(
-                      offset: Offset(0, 10.0),
-                      blurRadius: 10.0,
-                      color: Color.fromARGB(70, 0, 0, 0)
-                    ),
-                  
-                  ],
-
-                ),),
-               
-              ]
-            ),
-        Center(
-          child: Container(
-            decoration:  BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(30.0),
-             
-            ),
-            margin: const EdgeInsets.all(50),
-            width: 320,
-            height: 400,
-            child:    Column(
-              children: [
-                const SizedBox(
-                  height: 20,
+            child: Column(children: [
+              GestureDetector(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(20, 20, 0, 0),
+                          child:
+                              Image(image: AssetImage("assets/back_arrow.png")),
+                        )
+                      ])),
+              const SizedBox(
+                width: 0,
+                height: 30,
+              ),
+              const Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                Text(
+                  "HomeMate",
+                  style: TextStyle(
+                    fontSize: 45,
+                    fontFamily: 'Poppins',
+                    color: Colors.white,
+                    shadows: <Shadow>[
+                      Shadow(
+                          offset: Offset(0, 10.0),
+                          blurRadius: 10.0,
+                          color: Color.fromARGB(70, 0, 0, 0)),
+                    ],
+                  ),
                 ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                   const Text("Join", style: TextStyle(
-                        fontSize: 25,
-                        fontFamily: 'Poppins',
+              ]),
+              Center(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                  margin: const EdgeInsets.all(50),
+                  width: 320,
+                  height: 400,
+                  child: Column(
+                    children: [
+                      const SizedBox(
+                        height: 20,
                       ),
-                    ),
-                    Form(  
-                      key: _formKey,  
-                      child: Column(  
-                        crossAxisAlignment: CrossAxisAlignment.start,  
+                      Column(
                         mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          const SizedBox(height: 25,),
-                          SizedBox(
-                            width: 250,
-                            
-                            child:TextFormField(  
-                            decoration: const InputDecoration(  
-                          
-                              hintText: 'Enter your Dashboard ID',  
-                              labelText: 'Dashboard ID',  
-                              
-                            )
-                          )
-   
+                        children: [
+                          const Text(
+                            "Join",
+                            style: TextStyle(
+                              fontSize: 25,
+                              fontFamily: 'Poppins',
+                            ),
                           ),
-                          const SizedBox(height: 10,),
-                          SizedBox(
-                            width: 250,
-                            
-                            child:TextFormField(  
-                            decoration: const InputDecoration(  
-                          
-                              hintText: 'Enter your email',  
-                              labelText: 'Email',  
-                              
-                            )
-                          )
-   
-                          ),
-                          const SizedBox(height: 10,),
-                          SizedBox(
-                            width: 250,
-                            child:TextFormField(  
-                            decoration: const InputDecoration(  
-                          
-                              hintText: 'Enter your password',  
-                              labelText: 'Password',  
-                              
-                            )
-                          )
-                          ), 
-                          const SizedBox(height: 35,),
-                          SizedBox(
-                            width: 250,
-                            child:  Row(
+                          Form(
+                            key: _formKey,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
                               mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Center(
-                                  
-                                  child: ElevatedButton(onPressed: (){}, style: ElevatedButton.styleFrom(
-                                      backgroundColor: Color.fromARGB(255, 94, 91, 255),
-                                      fixedSize: Size(170, 50),
-
-                                  ), child: const Text("Join", style: TextStyle(
-                                    fontSize: 20,
-                                 
-                                  ),),
+                              children: <Widget>[
+                                const SizedBox(
+                                  height: 25,
+                                ),
+                                SizedBox(
+                                  width: 250,
+                                  child: TextFormField(
+                                    controller: _dashboardIdController,
+                                    decoration: const InputDecoration(
+                                      hintText: 'Enter your Dashboard ID',
+                                      labelText: 'Dashboard ID',
+                                    ),
+                                    validator: (value) {
+                                      if (value!.isEmpty) {
+                                        return 'Please enter a dashboard ID';
+                                      }
+                                      return null;
+                                    },
                                   ),
-                                )
+                                ),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                SizedBox(
+                                  width: 250,
+                                  child: TextFormField(
+                                    controller: _emailController,
+                                    decoration: const InputDecoration(
+                                      hintText: 'Enter your email',
+                                      labelText: 'Email',
+                                    ),
+                                    validator: (value) {
+                                      if (value!.isEmpty) {
+                                        return 'Please enter an email';
+                                      }
+                                      return null;
+                                    },
+                                  ),
+                                ),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                SizedBox(
+                                  width: 250,
+                                  child: TextFormField(
+                                    controller: _passwordController,
+                                    decoration: const InputDecoration(
+                                      hintText: 'Enter your password',
+                                      labelText: 'Password',
+                                    ),
+                                    validator: (value) {
+                                      if (value!.isEmpty) {
+                                        return 'Please enter a password';
+                                      }
+                                      return null;
+                                    },
+                                  ),
+                                ),
+                                const SizedBox(
+                                  height: 35,
+                                ),
+                                SizedBox(
+                                    width: 250,
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        Center(
+                                          child: isLoading ? const CircularProgressIndicator() : ElevatedButton(
+                                            onPressed: isLoading ? null : _joinDashboard,
+                                            style: ElevatedButton.styleFrom(
+                                              backgroundColor: Color.fromARGB(
+                                                  255, 94, 91, 255),
+                                              fixedSize: Size(170, 50),
+                                            ),
+                                            child: const Text(
+                                              "Join",
+                                              style: TextStyle(
+                                                fontSize: 20,
+                                              ),
+                                            ),
+                                          ),
+                                        )
+                                      ],
+                                    )),
                               ],
-                         )
-                          ),   
-                          
-                           
-                        ], 
-
-                      ), 
-                       
-                    )  
-                    
-                  ],
-                  
-                )
-              ],
-            ),
-
-          ),
-        ),
-        
-      
-      
-
-      ])
-     
-    )
-   );
-    
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ])));
   }
 }
 
 class GradientText extends StatelessWidget {
   const GradientText(
-    this.text, {super.key, 
+    this.text, {
+    super.key,
     required this.gradient,
     this.style,
   });
